c4117c4d957dcf359ded16fec4f6bbca4231d9b9
Roberto Oliveira <robertoufpa19@gmail.com>
2023-06-19 14:12:23 -0300

    arvore nao recursiva

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+
+int main(){
+
+        // primeira arvore
+         Arv* arvoreBinariaTeste =
+          // 2+3
+        arv_cria('+',
+                  arv_cria('2', arv_criavazia(), arv_criavazia()),
+                  arv_cria('3', arv_criavazia(), arv_criavazia()));
+
+
+      // segunda arvore
+           // (2+3)*(3-6)
+         Arv* arvoreBinariaTeste2=
+
+             arv_cria('*',
+                          arv_cria('+', arv_cria('2',arv_criavazia(),arv_criavazia()),
+                                   arv_cria('4',arv_criavazia(), arv_criavazia())),
+                                    arv_cria('-',arv_cria('3',arv_criavazia(), arv_criavazia()),
+                                            arv_cria('6',arv_criavazia(), arv_criavazia())) );
+
+
+    printf("Imprime Arvore Teste 1 \n");
+	arv_imprime(arvoreBinariaTeste);
+
+	printf("\n");
+	printf("\n");
+	    printf("Imprime expressao em Ordem \n");
+	    imprime_expressao(arvoreBinariaTeste);
+
+	printf("\n");
+	printf("\n");
+
+
+
+		printf("Imprime Arvore Binaria Teste 2\n");
+	arv_imprime(arvoreBinariaTeste2);
+	printf("\n");
+	printf("\n");
+	  printf("Imprime expressao em Ordem  \n");
+	    imprime_expressao(arvoreBinariaTeste2);
+
+	printf("\n");
+	printf("\n");
+
+
+
+
+}
---
+++ pilha.h
@@ -0,0 +1,11 @@
+
+
+typedef struct pilha Pilha;
+
+Pilha* cria_pilha (void);
+void push (char c, Arv* e, Arv* d);
+void pop (Arv* a);
+void pilha_vazia (Arv* a);
+void pilha_libera (Arv* a);
+
+
